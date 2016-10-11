ea23787406b279abf2d5175343b43e795ceaa079
NARUTO\donin_000 <doninthailand@gmail.com>
2016-10-11 17:39:15 +0100

    Initial Commit

---
+++ DungeonEscape/Source/DungeonEscape/DungeonEscapeGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "DungeonEscapeGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DUNGEONESCAPE_API ADungeonEscapeGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
