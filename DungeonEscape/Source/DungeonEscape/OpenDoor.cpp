d3719d76850628fd33e5b124972eac9af7adc337
NARUTO\donin_000 <doninthailand@gmail.com>
2016-10-23 23:15:02 +0100

    Started working on walls.

--- DungeonEscape/Source/DungeonEscape/OpenDoor.cpp
+++ DungeonEscape/Source/DungeonEscape/OpenDoor.cpp
@@ -20,9 +20,17 @@ UOpenDoor::UOpenDoor()
 void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
-	
+	// Find the owning Actor
+	auto Owner = GetOwner();
+	// Log some messages so we know what is going on when we test it
+	FString ObjectName = Owner->GetName();
+	FString ObjectRotation = Owner->GetTransform().GetRotation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s has rotation of %s "), *ObjectName, *ObjectRotation );
+	UE_LOG(LogTemp, Warning, TEXT("Now I'm going to fuck with the Z-axis "));
+	// Create a rotator
+	FRotator NewRotation = FRotator(0.0f, 90.0f, 0.0f);
+	//Set the door rotation
+	Owner->SetActorRotation(NewRotation);
 }
 
 
